cf63f9210422983632f64ef7c1ef833f0de2374f
RomanLeibiuk <92354812+RomanLeibiuk@users.noreply.github.com>
2022-01-16 03:07:56 +0200

    Яко-тако ледве-ледве)

--- js/modal-header.js
+++ js/modal-header.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector('[data-mobile-open]'),
-      closeModalBtn: document.querySelector('[data-mobile-close]'),
-      modal: document.querySelector('[data-mobile]'),
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal-header]'),
     };
   
   
