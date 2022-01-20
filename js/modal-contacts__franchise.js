6f6ed0acf6d39db6accb142b0d5916d2151cd56f
RomanLeibiuk <92354812+RomanLeibiuk@users.noreply.github.com>
2022-01-20 08:35:14 +0200

    1

--- js/modal-contacts__franchise.js
+++ js/modal-contacts__franchise.js
@@ -15,3 +15,16 @@ const refs = {
     refs.modal.classList.toggle('is-hidden-fr');
   }
 })();
+// const refs = {
+//     openModalBtn: document.querySelector('[modal-open-about]'),
+//     closeModalBtn: document.querySelector('[modal-close-about]'),
+//     modal: document.querySelector('[modal-about]'),
+//     scrollBtn: document.querySelector('.scroll-up'),
+//   };
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+//   function toggleModal() {
+//     refs.modal.style.transition = 'opacity 500ms cubic-bezier(0.88, -0.13, 0.58, 1) 0s';
+//     refs.modal.classList.toggle('is-open');
+//   }
+// })();
