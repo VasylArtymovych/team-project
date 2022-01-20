6987e21a4c11b98d193dab452db1ef5def237572
RomanLeibiuk <92354812+RomanLeibiuk@users.noreply.github.com>
2022-01-20 08:42:33 +0200

    1

--- js/modal-contacts__franchise.js
+++ js/modal-contacts__franchise.js
@@ -1,31 +1,15 @@
 
-(() => {  
-const refs = {
-    openModalBtn: document.querySelector('[modal-open-fr]'),
-    closeModalBtn: document.querySelector('[modal-close-fr]'),
-    modal: document.querySelector('[modal-fr]'),
-  
-
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open-about1]'),
+    closeModalBtn: document.querySelector('[modal-close-about1]'),
+    modal: document.querySelector('[modal-about1]'),
+   
   };
-
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-fr');
+    refs.modal.style.transition = 'opacity 500ms cubic-bezier(0.88, -0.13, 0.58, 1) 0s';
+    refs.modal.classList.toggle('is-open');
   }
 })();
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[modal-open-about1]'),
-//     closeModalBtn: document.querySelector('[modal-close-about1]'),
-//     modal: document.querySelector('[modal-about1]'),
-   
-//   };
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-//   function toggleModal() {
-//     refs.modal.style.transition = 'opacity 500ms cubic-bezier(0.88, -0.13, 0.58, 1) 0s';
-//     refs.modal.classList.toggle('is-open');
-//   }
-// })();
